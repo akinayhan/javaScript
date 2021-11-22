4251100e7d52bc2c9bce840e6a04aa28a119639c
akın ayhan <akinayhan94@gmail.com>
2021-11-23 00:02:26 +0300

    22.11

---
+++ src/data/users.js
@@ -0,0 +1,57 @@
+export const users = [
+    {
+        id:1,
+        firstName: "Akın",
+        lastName: "Ayhan",
+        city:"Bursa",
+        age:27,
+        creditCardNumber:"123456",
+        type:"customer"
+    },
+    {
+        id:6,
+        firstName: "Abdul",
+        lastName: "Kader",
+        city:"Van",
+        age:30,
+        creditCardNumber:"123456",
+        type:"customer"
+    },
+    {
+        id:2,
+        firstName: "Ali",
+        lastName: "Veli",
+        city:"Ankara",
+        age:21,
+        creditCardNumber:"654321",
+        type:"customer"
+    },
+    {
+        id:3,
+        firstName: "Hasan",
+        lastName: "Hüseyin",
+        city:"İstanbul",
+        salary:8000,
+        age:20,
+        type:"employee"
+    },
+    {
+        id:4,
+        firstName: "Ayşe",
+        lastName: "Fatma",
+        city:"İzmir",
+        age:25,
+        salary:17000,
+        type:"employee"
+    },
+    {
+        id:5,
+        firstName: "Can",
+        lastName: "Cemil",
+        city:"Eskişehir",
+        age:20,
+        salary:15000,
+        type:"employee"
+    }
+
+]
